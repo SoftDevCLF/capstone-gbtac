d87c2da26a39e0fc5f91ba9bb112efe0f0f31143
Temi Bankole <temibankole5@gmail.com>
2026-01-23 12:52:16 -0700

    Base setup of report page

--- app/(staff)/report/page.js
+++ app/(staff)/report/page.js
@@ -1,1 +1,35 @@
+import SecondaryNav from "@/app/_components/SecondaryNav";
+import Navbar from "@/app/_components/Navbar";
+import Footer from "@/app/_components/Footer";
 
+export default function Page() {
+  return (
+    <main className="bg-gray-50 min-h-screen">
+        <SecondaryNav />
+        <Navbar />
+      
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-10  dark:text-black" style={{ fontFamily: "var(--font-titillium)" }}>
+          Reports
+        </h1>
+
+
+        {/* LEFT: Dropdown + Calendar */}
+       <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+          <div className="col-span-1 p-6 bg-white shadow-md rounded-xl border space-y-6"></div>      
+        </div>
+
+        {/* Inline error message */}
+            {/* {errorMessage && (
+              <p className="mt-2 text-red-600 font-bold">{errorMessage}</p>
+            )} */}
+          </div>
+
+        {/* RIGHT: PDF Viewer */}
+          <div className="col-span-2">
+            {/* {showViewer && <PDFViewer pdfBlob={pdfBlob} onClear={handleClear} />} */}
+          </div>
+        <Footer />
+    </main>
+  );
+}
