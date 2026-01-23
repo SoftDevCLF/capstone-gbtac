e769a1ef60f081a6dd061c69eb74dadd18a09bc4
Temi Bankole <temibankole5@gmail.com>
2026-01-23 12:27:10 -0700

    Base header and footer

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,19 +1,29 @@
 import Image from "next/image";
+
 export default function Footer() {
   return (
-    <div className="w-full bg-[#6D2077] border-t border-[#6D2077] py-4">
-      <p className="text-right text-[#F1FAF5] ">
-        {new Date().getFullYear()}. Capstone Project for GBTAC, SAIT.
-      </p>
-      <div>
-        {" "}
-        <Image
-          src="/sait_extended_horizontal_reverse.png"
-          alt="Logo"
-          width={80}
-          height={80}
-        />
+    <footer className="w-full bg-[#1C232C] text-white py-4 px-6 mt-12">
+      <div className="flex items-center justify-between">
+        {/* Left - Social Icons */}
+        <div className="flex gap-4">
+          <a href="#" className="hover:opacity-80">
+            FB
+          </a>
+          <a href="#" className="hover:opacity-80">
+            IG
+          </a>
+          <a href="#" className="hover:opacity-80">
+            X
+          </a>
+        </div>
+
+        <p className="text-sm text-center grow">
+          Green Building Technology Access Centre - SAIT
+        </p>
+
+        {/* Right - Logo */}
+        <Image src="/sait-logo.png" alt="SAIT Logo" height={40} width={40} />
       </div>
-    </div>
+    </footer>
   );
 }
