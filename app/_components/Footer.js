f3aa8d51a3145115c9996c7d869bed4dbf8d0e76
SoftDevCLF <cintya.lara.f@gmail.com>
2026-02-03 11:01:30 -0700

    Update colors, direction and image size

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,19 +1,20 @@
 import Image from "next/image";
 export default function Footer() {
   return (
-    <div className="w-full bg-[#6D2077] border-t border-[#6D2077] py-4">
-      <p className="text-right text-[#F1FAF5] ">
-        {new Date().getFullYear()}. Capstone Project for GBTAC, SAIT.
-      </p>
+    <div className="w-full bg-[#6D2077] border-t border-[#6D2077] flex flex-row items-center justify-between py-3 sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
       <div>
         {" "}
         <Image
           src="/sait_extended_horizontal_reverse.png"
           alt="Logo"
-          width={80}
-          height={80}
+          width={200}
+          height={100}
         />
       </div>
+
+      <p className="text-right text-[#F1FAF5] ">
+        {new Date().getFullYear()}. Capstone Project for GBTAC, SAIT.
+      </p>
     </div>
   );
 }
