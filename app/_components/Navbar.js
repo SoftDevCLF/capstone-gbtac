4190287e9d3fbcdbb287269fc0fde0535c1c434b
SoftDevCLF <cintya.lara.f@gmail.com>
2026-02-08 19:46:38 -0700

    Fix name and logic

--- app/_components/Navbar.js
+++ app/_components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function SecondNavbar({
+export default function Navbar({
   displayHome = true,
   displayAbout = true,
   displayDashboards = false,
@@ -17,7 +17,6 @@ export default function SecondNavbar({
       "
     >
       <div className="max-w-7xl mx-auto">
-        {/* gap-x-8 FIXES spacing */}
         <ul className="flex items-center gap-x-8 text-16">
           {displayHome && (
             <li>
@@ -40,8 +39,6 @@ export default function SecondNavbar({
               <span className="cursor-pointer hover:opacity-70 transition">
                 Graphs
               </span>
-
-              {/* hover buffer */}
               <div className="absolute left-0 top-full h-2 w-full" />
 
               <ul
@@ -54,82 +51,83 @@ export default function SecondNavbar({
                 "
               >
                 <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  <Link
+                    href="/dashboards/water-level"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
                     Water Level
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  <Link
+                    href="/dashboards/energy"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
                     Energy
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  <Link
+                    href="/dashboards/ambient-temperature"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
                     Ambient Temperature
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  <Link
+                    href="/dashboards/wall-temperature"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
                     Wall Temperature
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                    Natural gas
-                  </a>
+                  <Link
+                    href="/dashboards/natural-gas"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Natural Gas
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                    Customizable graphs
-                  </a>
+                  <Link
+                    href="/dashboards/customizable-graphs"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Customizable Graphs
+                  </Link>
                 </li>
               </ul>
             </li>
           )}
 
           {displayReports && (
-            <li className="relative group">
-              <span className="cursor-pointer hover:opacity-70 transition">
+            <li>
+              <Link href="/about" className="hover:opacity-70 transition">
                 Reports
-              </span>
-
-              <div className="absolute left-0 top-full h-2 w-full" />
-
-              <ul
-                className="
-                  absolute left-0 top-full mt-2 w-44
-                  bg-white text-black shadow-lg
-                  opacity-0 invisible
-                  group-hover:opacity-100 group-hover:visible
-                  transition duration-150
-                "
-              >
-                <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                    Monthly
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                    Annual
-                  </a>
-                </li>
-              </ul>
+              </Link>
             </li>
           )}
 
           {displayAccountMngmt && (
             <li>
-              <a href="#" className="hover:opacity-70 transition">
+              <Link
+                href="/account-management"
+                className="hover:opacity-70 transition"
+              >
                 Account Management
-              </a>
+              </Link>
             </li>
           )}
           {displayDashboardMngmt && (
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+              <Link
+                href="/dashboard-management"
+                className="block px-4 py-2 hover:bg-gray-100"
+              >
                 Dashboard Management
-              </a>
+              </Link>
             </li>
           )}
         </ul>
