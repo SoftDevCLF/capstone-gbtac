f0b78ab6a0db76b279a48cc3ae999c48fa2c38db
SoftDevCLF <cintya.lara.f@gmail.com>
2026-03-13 13:58:00 -0600

    Merge with main

--- app/_components/DashboardLayout.js
+++ app/_components/DashboardLayout.js
@@ -2,7 +2,11 @@ import SecondaryNav from "./SecondaryNav";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function DashboardLayout({ title, children }) {
+export default function DashboardLayout({
+  title,
+  titleRight = null,
+  children,
+}) {
   return (
     <div className="flex flex-col min-h-screen bg-[#FdFdFd] font-sans">
       <SecondaryNav
@@ -18,7 +22,10 @@ export default function DashboardLayout({ title, children }) {
         p-8
       "
       >
-        <h1 className="text-2xl font-semibold text-gray-800 mb-6">{title}</h1>
+        <div className="mb-6 flex items-start justify-between gap-4">
+          <h1 className="text-3xl font-semibold text-gray-800">{title}</h1>
+          {titleRight}
+        </div>
 
         {children}
       </main>
